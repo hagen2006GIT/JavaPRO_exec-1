e408f2fb8c7590239d62eaf221b7729016687ba7
AKuzminykh <hagen2006-75@yandex.ru>
2024-06-18 12:16:28 +0300

    commit for approve

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import annotation.AfterSuite;
-import annotation.BeforeSuite;
-import annotation.CsvSource;
-import annotation.Test;
+import annotation.*;
 
 public class Animal {
     @BeforeSuite static void sleep() {
@@ -12,18 +9,31 @@ public class Animal {
     @AfterSuite static void walk() {
         System.out.println("Animal is walking");
     }
-
+// ************************** основные тесты
     @Test (priority=10) public void eat() {
         System.out.println("Animal is eating");
     }
     @Test public void drink() {
         System.out.println("Animal is drinking");
     }
-    @Test (priority = 4) public void run() {
+    @Test (priority = 3) public void run() {
         System.out.println("Animal is running");
     }
-
+    @Test (priority = 7) public void swim() {
+        System.out.println("Animal is swimming");
+    }
+// **************************
+    @BeforeTest public void fly() {
+        System.out.println("Animal is flying - @BeforeTest");
+    }
+    @BeforeTest public void land() {
+        System.out.println("Animal is landing - @BeforeTest");
+    }
+    @AfterTest public void takeOff() {
+        System.out.println("Animal is taking off - @AfterTest");
+    }
+// **************************
     @CsvSource(value = "10, Java, 20, true") public void jump(int p1,String p2,int p3,Boolean p4) {
-        System.out.println("p1 = "+p1+", p2 = "+p2+", p3 = "+p3+", p4 = "+p4);
+        System.out.println("CsvSource params:\n"+"\tp1 = "+p1+"\n\tp2 = "+p2+"\n\tp3 = "+p3+"\n\tp4 = "+p4);
     }
 }
--- src/test/java/org/example/TestRunner.java
+++ src/test/java/org/example/TestRunner.java
@@ -1,10 +1,6 @@
 package org.example;
 
-import annotation.AfterSuite;
-import annotation.BeforeSuite;
-import annotation.CsvSource;
-import annotation.Test;
-
+import annotation.*;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -17,13 +13,13 @@ public class TestRunner {
         runTests(Animal.class);
     }
     static void runTests(Class c) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
-        Class<Animal> animalClass=Animal.class;
-        Method[] arrMethod=animalClass.getDeclaredMethods();
-        Constructor<Animal> constructor=animalClass.getConstructor();
-        Animal animal= constructor.newInstance();
-        Map<String,Method> map=new TreeMap<String,Method>(); //map для упорядоченного списка выполнения методов
+        Method[] arrMethod= c.getDeclaredMethods();
+        Constructor<Animal> constructor=c.getConstructor();
+        Animal animal=constructor.newInstance();
+        Map<Integer,List<Method>> mapMainSuites=new TreeMap<Integer,List<Method>>(Collections.reverseOrder()); //map для выполнения методов в соответствии с их приоритетами
+        List<Method> mapBeforeTest=new ArrayList<>(); // список методов, выполняемых ПЕРЕД каждым тестом
+        List<Method> mapAfterTest=new ArrayList<>(); // список методов, выполняемых ПОСЛЕ каждого теста
 
-// посчитать количество методов с аннотациями @BeforeSuite и @AfterSuite
         for(Method method:arrMethod){
             Annotation[] annotations=method.getAnnotations();
             for (Annotation annotation : annotations) {
@@ -31,47 +27,79 @@ public class TestRunner {
                     if (!Modifier.isStatic(method.getModifiers())) {
                         throw new RuntimeException("Static methods only allowed with BeforeSuite annotation");
                     }
-                    if (map.get("00")!=null) {
+                    if (mapMainSuites.get(Integer.MAX_VALUE)!=null) {
                         throw new RuntimeException("Only one @BeforeSuite are allowed");
                     }
-                    map.put("00",method);
+                    mapMainSuites.put(Integer.MAX_VALUE,new ArrayList<Method>(Collections.singleton(method)));
                 } else if (annotation instanceof AfterSuite) {
                     if (!Modifier.isStatic(method.getModifiers())) {
                         throw new RuntimeException("Static methods only allowed with AfterSuite annotation");
                     }
-                    if (map.get("99")!=null) {
+                    if (mapMainSuites.get(Integer.MIN_VALUE) != null) {
                         throw new RuntimeException("Only one @AfterSuite are allowed");
                     }
-                    map.put("99",method);
-                } else if (annotation instanceof Test) {
-                    map.put(method.getAnnotation(Test.class).priority()+"-"+method.getName(),method);
+                    mapMainSuites.put(Integer.MIN_VALUE, new ArrayList<Method>(Collections.singleton(method)));
                 }
+// формирование списка на выполнение методов с аннотациями @Test
+                 else if (annotation instanceof Test) {
+                    if (mapMainSuites.get(method.getAnnotation(Test.class).priority())==null) {
+                        mapMainSuites.put(method.getAnnotation(Test.class).priority(),new ArrayList<Method>(Collections.singleton(method)));
+                    } else {
+                        mapMainSuites.get(method.getAnnotation(Test.class).priority()).add(method);
+                    }
+                }
+// проверка аннотаций @BeforeTest и @AfterTest и подготовка списка выполняемых методов в соответствии с семантикой аннотации
+                else if (annotation instanceof BeforeTest) {
+                    mapBeforeTest.add(method);
+                }
+                else if (annotation instanceof AfterTest) {
+                    mapAfterTest.add(method);
+                }
+// проверка на аннотацию @CsvSource
                 else if (annotation instanceof CsvSource) {
                     String[] params=method.getAnnotation(CsvSource.class).value().split(",");
                     Class[] paramTypes = method.getParameterTypes();
                     Object[] paramsArray = new Object[paramTypes.length];
                     for (int i = 0; i < paramTypes.length; i++) {
-                        if (paramTypes[i].getName()=="java.lang.String") {
-                            paramsArray[i]=params[i];
-                        } else if (paramTypes[i].getName()=="int") {
-                            paramsArray[i]=Integer.parseInt(params[i].trim());
-                        } else if (paramTypes[i].getName()=="java.lang.Boolean") {
-                            paramsArray[i]=Boolean.parseBoolean(params[i]);
+                        switch (paramTypes[i].getName()) {
+                            case "java.lang.String":
+                                paramsArray[i] = params[i].trim();
+                                break;
+                            case "int":
+                                paramsArray[i] = Integer.parseInt(params[i].trim());
+                                break;
+                            case "java.lang.Boolean":
+                                paramsArray[i] = Boolean.parseBoolean(params[i]);
+                                break;
                         }
                     }
+                    System.out.println("*");
                     method.invoke(animal,paramsArray);
+                    System.out.println("*");
                 }
             }
         }
-// выполнение методов класса Animal
-//        Collections.sort(map.keySet(), new Comparator);
-//        for (Map.Entry<String,Method> entry : map.entrySet()) {
-//            System.out.println(map.get(entry.getKey()));
-//            entry.getValue().invoke(animal);
-//        }
-/*        assert beforeSuiteMethod != null;
-        beforeSuiteMethod.invoke(animal);
-        assert afterSuiteMethod != null;
-        afterSuiteMethod.invoke(animal);*/
+// выполнение методов класса Animal в порядке убывания приоритетов аннотации @Test
+        for (Map.Entry<Integer,List<Method>> entry : mapMainSuites.entrySet()) {
+            for (Method method : entry.getValue()) {
+    // методы ПЕРЕД каждым тестом
+                if (!mapBeforeTest.isEmpty()
+                        && !method.isAnnotationPresent(BeforeSuite.class)
+                        && !method.isAnnotationPresent(AfterSuite.class)) {
+                    for (Method methodBeforeTest : mapBeforeTest) {
+                        methodBeforeTest.invoke(animal);
+                    }
+                }
+                method.invoke(animal); // выполнение основного теста
+    // методы ПОСЛЕ каждого теста
+                if (!mapBeforeTest.isEmpty()
+                        && !method.isAnnotationPresent(AfterSuite.class)
+                        && !method.isAnnotationPresent(BeforeSuite.class)) {
+                    for (Method methodAfterTest : mapAfterTest) {
+                        methodAfterTest.invoke(animal);
+                    }
+                }
+            }
+        }
     }
 }
