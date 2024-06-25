9206904f645aac20400604e8f7f480530b2ac42e
AKuzminykh <hagen2006-75@yandex.ru>
2024-06-25 10:14:10 +0300

    commit 3 (for approve)

--- src/test/java/org/example/StreamRunner.java
+++ src/test/java/org/example/StreamRunner.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.sql.Array;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -10,105 +9,94 @@ public class StreamRunner {
     public static void main(String[] args) {
 
         System.out.println("Изначальный список целых чисел: " +
-                Stream.of(5, 2, 10, 9, 4, 3, 10, 1, 13).collect(Collectors.toList()) + "\n");
+            Stream.of(5, 2, 10, 9, 4, 3, 10, 1, 13).collect(Collectors.toList()) + "\n");
 
         System.out.println("1. Удаление дубликатов: " +
-                Stream.of(5, 2, 10, 9, 4, 3, 10, 1, 13)
-                        .distinct()
-                        .map(it -> it + " ")
-                        .collect(Collectors.toList())
-                )
+            Stream.of(5, 2, 10, 9, 4, 3, 10, 1, 13)
+                    .distinct()
+                    .map(it -> it + " ")
+                    .collect(Collectors.toList())
+            )
         ;
 
         System.out.println("2. 3-е наибольшее число = " +
-                Stream.of(5, 2, 10, 9, 4, 3, 10, 1, 13)
-                    .sorted(Comparator.reverseOrder())
-                    .limit(3)
-                    .min(Integer::compare)
-                    .get()
+            Stream.of(5, 2, 10, 9, 4, 3, 10, 1, 13)
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .min(Integer::compare)
+                .get()
         );
 
         System.out.println("2. 3-е наибольшее \"уникальное\" число = " +
-                Stream.of(5, 2, 10, 9, 4, 3, 10, 1, 13)
-                    .distinct()
-                    .sorted(Comparator.reverseOrder())
-                    .skip(2)
-                    .limit(1)
-                    .findFirst()
-                    .get()
+            Stream.of(5, 2, 10, 9, 4, 3, 10, 1, 13)
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(2)
+                .limit(1)
+                .findFirst()
+                .get()
         );
 
         List<Person> persons = new ArrayList<>(
-                Arrays.asList(
-                    new Person("Alexandr", 50, Position.ENGINEER)
-                    ,new Person("Bob", 20, Position.MANAGER)
-                    ,new Person("Smith", 30, Position.ENGINEER)
-                    ,new Person("Michael", 40, Position.DIRECTOR)
-                    ,new Person("Arnold", 65, Position.ENGINEER)
-                    ,new Person("Max", 27, Position.ENGINEER)
-                    ,new Person("John", 70, Position.ENGINEER)
-                )
+            Arrays.asList(
+                new Person("Alexandr", 50, Position.ENGINEER)
+                ,new Person("Bob", 20, Position.MANAGER)
+                ,new Person("Smith", 30, Position.ENGINEER)
+                ,new Person("Michael", 40, Position.DIRECTOR)
+                ,new Person("Arnold", 65, Position.ENGINEER)
+                ,new Person("Max", 27, Position.ENGINEER)
+                ,new Person("John", 70, Position.ENGINEER)
+            )
         );
         System.out.println("\nСписок сотрудников (Person): "+persons
-                .stream()
-                .map(it -> it.getName()+"-"+it.getAge()+"-"+it.getPosition())
-                .collect(Collectors.toList()));
+            .stream()
+            .map(it -> it.getName()+"-"+it.getAge()+"-"+it.getPosition())
+            .collect(Collectors.toList()));
 
         System.out.println("4. 3 самых старших сотрудников с должностью «Инженер», в порядке убывания возраста = " +
-                persons.stream()
-                        .filter(it -> it.getPosition()==Position.ENGINEER)
-                        .sorted(Comparator.comparingInt(Person::getAge).reversed())
+            persons.stream()
+                .filter(it -> it.getPosition()==Position.ENGINEER)
+                .sorted(Comparator.comparingInt(Person::getAge).reversed())
 // в мапе возраст в скобках добавил только для наглядности при оценки результата, алгоритм не аффектит
-                        .map(it -> it.getName()+" ("+it.getAge()+")")
-                        .limit(3)
-                        .collect(Collectors.toList()));
+                .map(it -> it.getName()+" ("+it.getAge()+")")
+                .limit(3)
+                .collect(Collectors.toList()));
 
         System.out.println("5. Средний возраст ENGINEER = " +
-                persons.stream()
-                        .filter(it->it.getPosition()==Position.ENGINEER)
-                        .collect(Collectors.averagingInt(Person::getAge)
-                )
+            persons.stream()
+                    .filter(it->it.getPosition()==Position.ENGINEER)
+                    .collect(Collectors.averagingInt(Person::getAge)
+            )
         );
 
         List<String> longWords = new ArrayList<String>
-                (Arrays.asList("АвтоМотоАелоФотоТелеРадиоМонтер"
-                        ,"ВодоГрязеТорфоПарафиноЛечение"
-                        ,"ЧетырёхСотПятидесятиСемиМиллиметровое"));
+            (Arrays.asList("АвтоМотоАелоФотоТелеРадиоМонтер"
+                    ,"ВодоГрязеТорфоПарафиноЛечение"
+                    ,"ЧетырёхСотПятидесятиСемиМиллиметровое"));
         System.out.println("\n6. Самое длинное слово: "+longWords
-                .stream()
-                .max(Comparator.comparingInt(String::length))
-                .get()
-                +" из списка "+longWords
+            .stream()
+            .max(Comparator.comparingInt(String::length))
+            .get()
+            +" из списка "+longWords
         );
 
         Map<String, Long> mapWords = Arrays.stream("один два один три два пять один два восемь десять один три один десять".split(" "))
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         System.out.println("\n7. Количество повторяющихся слов: "+mapWords);
 
         System.out.println("\n8. Список в порядке увеличения длины слова: "+
-                Arrays.stream("в два а восемнадцать б семь тысяча доля целое".split(" "))
-                    .sorted()
-                    .sorted(Comparator.comparingInt(String::length))
-                    .toList()
+            Arrays.stream("в два а восемнадцать б семь тысяча доля целое".split(" "))
+                .sorted()
+                .sorted(Comparator.comparingInt(String::length))
+                .toList()
         );
 
-        String[] arr = {"один два три четыре пять","аб абв абвг абвгд абвгде","домашка по курсу java pro","задача номер девять stream api"};
-//        String[][] arr2 = new String[arr.length][]{{"один два три четыре пять"}};
-        String[][] arr2 = new String[][]{{"один два три четыре пять"}
-                                        ,{"аб абв абвг абвгд"}
-        };
-//        for (int i = 0; i < arr.length; i++) {
-//            System.arraycopy(arr[i].split(" "), 0, arr2[i], 0, arr[i].split(" ").length);
-//        }
-        System.out.println(Arrays.stream(
-                Arrays.stream(arr)
-                    .flatMap((p)->Arrays.asList(p.split(" "))
-                            .stream())
-                                .toArray(String[]::new)
-                )
-                .sorted(Comparator.comparingInt(String::length).reversed())
-                .findFirst()
-                .get()
+        String[] arr = {"один два три четыре пять","аб абв абвг абвгд абвгде","домашка по курсу java proPROproPROproPRO","задача номер девять stream api proPROproPROpro111"};
+        System.out.println("\nСамое длинное слова из массива строк: "+
+            Arrays.stream(arr) // развернул массив в Stream<String>
+            .flatMap((p) -> Arrays.stream(p.split(" ")))
+            .max(Comparator.comparingInt(String::length))
+            .get()
         );
     }
 }
